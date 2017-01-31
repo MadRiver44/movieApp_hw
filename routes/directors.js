4ca0e237b56ed02ac40276a400adf17f06ada9b8
Kevin Turney <super996@optonline.net>
2017-01-31 13:24:11 -0500

    first route renders list of all directors

--- routes/directors.js
+++ routes/directors.js
@@ -2,9 +2,18 @@ var express = require('express');
 var router = express.Router();
 var models = require('../db/models/index');
 
+// basic route for directors, when /directors is called
+// we go to directors.ejs and pass title in to render "Directors"
 router.get('/', function(req, res){
-  res.render('directors', {title: "Directors"})
+  models.Director.findAll({})
+    .then(function(director) {
+      res.render('directors', {
+        title : "Directors",
+        director: director
+      });
+    });
 });
+
 
 
 router.get('/:id', function(req, res, next) {
