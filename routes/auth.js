111aee226fdb3ac91d2e3e7e39a6b990d480a8b9
Steven Gallo <steven.gallo@outook.com>
2017-01-31 13:46:54 -0500

    add user profile page route, loginRequired helper method Fixes #3

--- routes/auth.js
+++ routes/auth.js
@@ -25,7 +25,7 @@ router.get('/login', authHelpers.loginRedirect, (req, res) => {
 });
 
 router.post('/login', passport.authenticate('local', {
-    successRedirect: '/user',
+    successRedirect: '/user/',
     failureRedirect: '/auth/login',
     failureFlash: true
 }));
