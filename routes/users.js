16b4ecd193f843c49f49b471deded4447d8e021a
Kevin Turney <super996@optonline.net>
2017-02-01 12:33:27 -0500

    added secret key, express-session

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,7 @@ const authHelpers = require('../auth/auth-helpers');
 router.get('/', authHelpers.loginRequired, (req, res, next) => {
     res.render('user/index', {
         user: req.user.dataValues
+      });
     });
 
 module.exports = router;
